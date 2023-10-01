581bff755811ea08d8d3c4cfe52d5e854f57631a
Iheanacho Chukwu <iheanacho.chukwu@outlook.com>
2023-10-01 00:31:15 +0000

    Azure VM using ARM

---
+++ ArmTemplates/Infrastructure/script.ps1
@@ -0,0 +1,23 @@
+﻿Install-Module -Name Az -AllowClobber -Force -Scope CurrentUser
+Get-ExecutionPolicy -List
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Import-Module Az
+$subscriptionId = "4f3f4052-a29a-4988-9f42-8c20c81ea86d"
+Connect-AzAccount -Subscription $subscriptionId
+
+
+# Create the resource group
+$resourceGroupName = "Accruent-Assessment-RG"
+$location = "westeurope"
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+$deploymentName = "AccruentDeployment"
+$templateFile = "template.json"
+$parameterFile = "parameters.json"
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+                             -Name $deploymentName `
+                             -TemplateFile $templateFile `
+                             -TemplateParameterFile $parameterFile
+
+
+
